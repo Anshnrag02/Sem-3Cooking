d1af901f24ddb2f448bbcd2e3a7b75e0cd155be9
Ansh Anurag <anxhnurag@gmail.com>
2021-09-10 20:14:03 +0530

    more codes

--- DS Lab/merge.c
+++ DS Lab/merge.c
@@ -1,52 +1,58 @@
 #include<stdio.h>
 
 void merge(int *a, int n, int *b, int m, int *c){
-	int i=0;
-	int j=0;
+	int i=n-1;
+	int j=m-1;
 	int k =0;
-	while(i<n && j<m){
-		if(a[i]<b[j]){
-			c[k++]=a[i++];
+	while(i>=0 && j>=0){
+		if(a[i]>b[j]){
+			c[k++]=a[i--];
 		}
-		else if(b[j]<a[i]){
-			c[k++]=b[j++];	
+		else if(b[j]>a[i]){
+			c[k++]=b[j--];	
 		}
 		else{
 		
-			c[k++]=a[i++];
-			c[k++]=b[j++];
+			c[k++]=a[i--];
+			c[k++]=b[j--];
 		}
 	}
 	
-	while(i<n){
-		c[k++]=a[i++];
+	while(i>=0){
+		c[k++]=a[i--];
 	}
 	
-	while(j<m){
-		c[k++]=b[j++];
+	while(j>=0){
+		c[k++]=b[j--];
 	}
+}
+
+void printReverse(int * a , int startIndex, int endIndex){
+	if(endIndex<startIndex){
+		return;
+	}
+	printf("%d ",a[endIndex]);
+	printReverse(a,startIndex,endIndex-1);
+}
+
+int isuppertriangular(struct sparse s[]){
+    int i=1;
+    for(i=1;i<=s[0].value;i++){
+        if(s[i].row>s[i].col){
+            return 0;
+        }
+    }
+    return 1;
 }
 
 int main(){
 	int n;
 	int a[100];
-	int m;
-	int b[100];
-	int c[200];
 	printf("Enter number of elements in array 1:\n");
 	scanf("%d",&n);
 	printf("Enter the elements\n");
 	for(int i =0; i<n; i++){
 		scanf("%d",&a[i]);
 	}
-	printf("Enter number of elements in array 2:\n");
-	scanf("%d",&m);
-	printf("Enter the elements\n");
-	for(int i =0; i<m; i++){
-		scanf("%d",&b[i]);
-	}
-	merge(a,n,b,m,c);
-	for(int i = 0  ; i < m+n ; i++){
-		printf("%d ", c[i]);
-	}
+	printReverse(a,0,n-1);//call from Main Program
 }
