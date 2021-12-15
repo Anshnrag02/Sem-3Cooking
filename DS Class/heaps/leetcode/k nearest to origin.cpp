574302db3e7d2d3e8152f76ff862d020a2b21f18
Ansh Anurag <anxhnurag@gmail.com>
2021-12-15 20:44:18 +0530

    s

--- DS Class/heaps/leetcode/k nearest to origin.cpp
+++ DS Class/heaps/leetcode/k nearest to origin.cpp
@@ -59,3 +59,38 @@ public:
         return ans;
     }
 };
+
+/*
+class Solution {
+public:
+    class Compare{
+        public:
+            bool operator()(vector<int>a,vector<int>b){
+                return a[0]*a[0]+a[1]*a[1]<b[0]*b[0]+b[1]*b[1];
+            }
+    };
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        priority_queue<vector<int>,vector<vector<int>>,Compare>heap;
+        for(int i=0; i<k; i++){
+            heap.push(points[i]);
+        }
+        for(int i=k; i<points.size(); i++){
+            int x=points[i][0];
+            int y=points[i][1];
+            int x1=heap.top()[0];
+            int y1=heap.top()[1];
+            if(x*x+y*y<x1*x1+y1*y1){
+                heap.pop();
+                heap.push(points[i]);
+            }
+        }
+        vector<vector<int>>ans;
+        while(heap.size()){
+            ans.push_back(heap.top());
+            heap.pop();
+        }
+        return ans;
+    }
+};
+
+*/
