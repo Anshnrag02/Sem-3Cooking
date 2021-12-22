a6895bf975d7ac04543d2fc618b827f0e56ecc82
Ansh Anurag <anxhnurag@gmail.com>
2021-12-22 21:28:50 +0530

    added code

--- DS Class/array/leetcode/stocks2.cpp
+++ DS Class/array/leetcode/stocks2.cpp
@@ -1,14 +1,4 @@
-class Solution {
-public:
-    int maxProfit(vector<int>& prices) {
-        int sum =0 ;
-        for(int i =0 ; i < prices.size()-1 ; ++i){
-            if(prices[i+1]-prices[i]>0)
-                sum+=(prices[i+1]-prices[i]);
-        }
-        return sum;
-    }
-};
+#include<bits/stdc++.h>
 
 class Solution {
 public:
