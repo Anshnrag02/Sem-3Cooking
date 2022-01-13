ed8e999194cba3a164453030a8bd275e7a1c4952
Ansh Anurag <anxhnurag@gmail.com>
2022-01-13 17:38:56 +0530

    more work

--- DS Class/DP/mindiffssq.cpp
+++ DS Class/DP/mindiffssq.cpp
@@ -1,40 +1,60 @@
-#include<bits/stdc++.h>
+// { Driver Code Starts
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int n,sum=0; 
-    cin>>n;
-    vector<int>v(n);
-    for(int i = 0 ; i < n ; i++){
-        cin>>v[i];
-        sum+=v[i];
-    }
-    vector<vector<bool>>dp(n+1,vector<bool>(sum+1,false));
-    dp[0][sum]=true;
+ // } Driver Code Ends
+class Solution{
 
-    for(int i = 1 ; i<=n ; ++i){
-        for(int j = 0; j<=sum ; j++){
-            dp[i][j]=dp[i-1][j];
-            if(j-2*v[i-1]>=0 and dp[i-1][j]){
-                dp[i][j-2*v[i-1]] = 1;
+  public:
+	int minDifference(int  v[], int n)  { 
+	    // Your code goes here
+	    int sum=0; 
+        for(int i = 0 ; i < n ; i++){
+            sum+=v[i];
+        }
+        vector<vector<bool>>dp(n+1,vector<bool>(sum+1,false));
+        dp[0][sum]=true;
+    
+        for(int i = 1 ; i<=n ; ++i){
+            for(int j = 0; j<=sum ; j++){
+                dp[i][j]=dp[i-1][j];
+                if(j-2*v[i-1]>=0 and dp[i-1][j]){
+                    dp[i][j-2*v[i-1]] = 1;
+                }
             }
         }
-    }
-
-    for(int j = 0 ; j<=sum ; j++){
-        // cout<<dp[n][j];
-        if(dp[n][j]==true){
-            cout<<j;
-            break;
+    
+        for(int j = 0 ; j<=sum ; j++){
+            // cout<<dp[n][j];
+            if(dp[n][j]==true){
+                return j;
+            }
         }
-    }
+	} 
+};
 
-    cout<<endl;
-    for(int i = 0 ; i <= n ; i++ ){
-        for(int j = 0 ; j<= sum ; j++){
-            cout<<dp[i][j];
-        }
-        cout<<endl;
-    }
 
-}
+// { Driver Code Starts.
+int main() 
+{
+   
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        int a[n];
+        for(int i = 0; i < n; i++)
+        	cin >> a[i];
+
+       
+
+	    Solution ob;
+	    cout << ob.minDifference(a, n) << "\n";
+	     
+    }
+    return 0;
+}  // } Driver Code Ends
