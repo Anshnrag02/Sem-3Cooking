99fbe75a1218ad5a2ea6f75c0e9113c783c60374
Ansh Anurag <anxhnurag@gmail.com>
2022-01-18 10:40:12 +0530

    more code and more code

---
+++ DS Class/DP/matrix.cpp
@@ -0,0 +1,73 @@
+// { Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int maxSquare(int n, int m, vector<vector<int>> mat){
+        // code here
+        vector<vector<int>>dp(n+1, vector<int>(m+1,0));
+        for(int i = 0 ; i<=n ; ++i){
+            for(int j =0 ; j<=m ; j++){
+                if(j==0 or i==0){
+                    dp[i][j]=0;
+                }
+                else{
+                    int a=0 , b=0, c=0;
+                    
+                    if(i>0){
+                        a=dp[i-1][j];
+                    }
+                    
+                    if(j>0){
+                        b=dp[i][j-1];
+                    }
+                    
+                    if(i>0  and j>0){
+                        c=dp[i-1][j-1];
+                    }
+                    
+                    if(mat[i-1][j-1]==1){
+                        dp[i][j] = 1 + min({a,b,c});
+                    }else{
+                        dp[i][j]=0;
+                    }
+                }
+            }
+        }
+        for(int i = 0 ; i<=n; i++){
+            for(int j =0 ; j<=m ; ++j){
+                cout<<dp[i][j]<<" ";
+            }
+            cout<<"\n";
+        }
+
+        cout<<m;
+
+        return dp[n][m];
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t=1;
+    // cin>>t;
+
+    while(t--){
+        int n, m;
+        cin>>n>>m;
+        vector<vector<int>> mat(n, vector<int>(m, 0));
+        for(int i = 0;i < n*m;i++)
+            cin>>mat[i/m][i%m];
+        
+        Solution ob;
+        cout<<ob.maxSquare(n, m, mat)<<"\n";
+    }
+    return 0;
+}  // } Driver Code Ends
