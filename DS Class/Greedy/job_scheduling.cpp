e8ff0382e5766f374748f0ea432729864eeb2719
Ansh Anurag <anxhnurag@gmail.com>
2022-01-28 21:15:33 +0530

    started greedy

--- DS Class/Greedy/job_scheduling.cpp
+++ DS Class/Greedy/job_scheduling.cpp
@@ -5,25 +5,33 @@ int main(){
     int n;
     cin>>n;
     vector<vector<int>>a(n, vector<int>(2));
+    int m=INT_MIN;
+    for(int i = 0 ; i <  n ; ++i){
+        cin>>a[i][1];
+        m=max(m,a[i][1]);
+    }
     for(int i = 0 ; i <  n ; ++i){
         cin>>a[i][0];
     }
-    for(int i = 0 ; i <  n ; ++i){
-        cin>>a[i][1];
-    }
-    sort(a.begin(),a.end());
-    dequeue<int>q;
-    for(int i = 0 ; i < n; ++i){
-        if(q.size()==0){
-            q.push_back(i);
-        }else{
 
-            if(q.size()<a[i][0]){
-                q.push_back(i);
-            }else{
-                
+    vector<int>occupied(m+1,-1);
+
+    sort(a.begin(),a.end(),greater<>());
+    for(int i = 0 ; i < n ; i++){
+        for(int j = a[i][1] ; j > 0 ; j-- ){
+            if(occupied[j]==-1){
+                occupied[j]=i;
+                break;
             }
-
         }
     }
+
+    for(int i = 0 ; i <=m ; ++i){
+        if (occupied[i]>=0)
+        {
+            cout<<a[occupied[i]][0]<<" "<<a[occupied[i]][1]<<endl;
+        }
+        
+    }
+        
 }
