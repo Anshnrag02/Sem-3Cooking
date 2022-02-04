e430e53eb0b404379b81be2097645fbe5c6d31a3
Ansh Anurag <anxhnurag@gmail.com>
2022-02-04 17:16:09 +0530

    cf cf c f

---
+++ forces/gregarray.cpp
@@ -0,0 +1,115 @@
+//GODLEON'S CODE
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ff                first
+#define ss                second
+#define int        	  long long
+#define ll                long long
+#define ddouble           long double  
+#define pb 		  push_back
+#define mp 		  make_pair
+#define pii               pair<int , int>
+#define vi                vector<int>
+#define mii               map<int,int>
+#define gsort             greater<int>
+#define pqb               priority_queue<int>
+#define pqs               priority_queue<int , vi , greater<int>>
+#define setbits(x)        __builtin_popcountll(x)
+#define zrobits(x)        __builtin_ctzll(x)
+#define mod               1000000007
+#define mod1              mod
+#define mod2              1000000009
+#define inf               1e18
+#define ps(x,y)           fixed<<setprecision(y)<<x
+#define mk(arr,n,type)    type *arr = new type[n] ;
+#define test(t)           int t ; cin >> t ; while(t--)
+#define ifor(a,b)         for(int i = a ; i < b ; i++)
+#define dfor(a,b)         for(int i = b-1 ; i >= a ; i--)
+#define fio               ios_base::sync_with_stdio(0) ; cin.tie(0) ; cout.tie(0) ;
+mt19937                   rng(chrono::steady_clock::now().time_since_epoch().count());
+
+
+// check odd even : n & 1
+// check power of 2 : n & n - 1 == 0
+//flip xor
+//set or
+//unset and
+//for -ve no.s mod ( a % mod + mod ) % mod ; 
+
+/*
+ll power(ll x, ll y, ll p) 
+{ 
+	if(x==0 && y==0)
+	{
+		return 1;
+	}
+	ll res = 1;	 
+	x = x % p;  
+
+	if (x == 0) return 0; 
+
+	while (y > 0) 
+	{ 
+		if (y & 1) 
+			res = (res*x) % p; 
+		y = y>>1;
+		x = (x*x) % p; 
+	} 
+	return res; 
+} 
+*/
+
+int A[100001];
+int B[100001]; // record how much value to fix
+int C[100001]; // record how many operations
+int M[100001][3];
+
+
+
+int32_t main()
+{
+    fio;
+    // ll ttt=-1;
+	int t =1;
+	// cin>>t;
+    while(t--)
+    {
+        int n,m,k; cin>>n>>m>>k;
+        memset(A, 0, sizeof(A));
+        memset(B, 0, sizeof(B));
+        memset(C, 0, sizeof(C));
+        memset(M, 0, sizeof(M));
+        // vi a(n);
+        // vector<vi>lrd(m,vi&(3)),xy(k,vi&(2));
+        
+        ifor(0,n)
+            cin>>A[i];
+        
+        ifor(0,m)
+        {    
+            cin>>M[i][0]>>M[i][1]>>M[i][2];
+            M[i][0]--;
+            M[i][1]--;
+        }
+        ifor(0,k)
+        {
+            int a,b; cin>>a>>b;
+            a--;
+            C[a]++,C[b]--;
+        }
+
+        int temp = 0 ; 
+        ifor(0,m){
+            temp+=C[i];
+            B[M[i][0]]+= temp*M[i][2];
+            B[M[i][1]+1] -=temp*M[i][2];
+        }
+        temp = 0 ; 
+        ifor(0,n)
+        {
+            temp+=B[i];
+            cout<<temp+A[i]<<" ";
+        }
+    }
+}
