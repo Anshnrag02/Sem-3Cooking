f7db3330e0b8094a58b3f780ba836fd3119313b5
Ansh Anurag <anxhnurag@gmail.com>
2022-03-16 20:07:49 +0530

    vvvv

--- DS Class/DP/jumpmin.cpp
+++ DS Class/DP/jumpmin.cpp
@@ -19,3 +19,23 @@ int main(){
         cout<<dp[i]<<" ";
     }
 }
+
+// class Solution {
+// public:
+//     int jump(vector<int>& a) {
+//         int i = 0 ;
+//         int n = a.size();
+//         vector<int>dp(100000);
+//         for(int i = 0 ; i < n ; i++)
+//             dp[i]=INT_MAX;
+//         dp[0]=0;
+//         for(int i = 0 ; i < n-1; ++i){
+//             for(int j = i+1; j<=min(n-1,i+a[i]); j++){
+                
+//                     dp[j]=min(dp[i]+1,dp[j]);
+//             }
+//         }
+//         return dp[n-1];
+        
+//     }
+// };
