4b975053aa53ca2acb4aaa99bfdf499e89e3d92a
Ansh Anurag <anxhnurag@gmail.com>
2022-03-19 01:50:30 +0530

    Game Game

--- DS Class/DP/coingame.cpp
+++ DS Class/DP/coingame.cpp
@@ -6,14 +6,9 @@ int a[100000];
 
 int solve(int i = 0 , int j =n-1)
 {
-    if(i==j){
-        return dp[i][j]=a[i];
-    }
-
-    if(i+1==j)
-        return dp[i][j]=max(a[i],a[j]);
-
-
+    if(i>j)
+        return 0;
+    
     if(dp[i][j]==-1){
         dp[i][j]=max(a[i]+min(solve(i+2,j),solve(i+1,j-1)),a[j]+min(solve(i+1,j-1),solve(i,j-2)));
     }
--- DS Class/DP/tempCodeRunnerFile.cpp
+++ DS Class/DP/tempCodeRunnerFile.cpp
@@ -1,3 +1,10 @@
-if(n==4 and m==6){
-                    //     cout<<"hello";
-                    // }
+int solve(int i = 0 , int j =n-1)
+{
+    if(i>j)
+        return 0;
+    
+    if(dp[i][j]==-1){
+        dp[i][j]=max(a[i]+min(solve(i+2,j),solve(i+1,j-1)),a[j]+min(solve(i+1,j-1),solve(i,j-2)));
+    }
+    return dp[i][j];
+}
