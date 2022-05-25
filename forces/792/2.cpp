9a4d7d07d950f113fdca925b25d0490c14afa5ec
Ansh Anurag <anxhnurag@gmail.com>
2022-05-25 15:12:29 +0530

    CF

---
+++ forces/792/2.cpp
@@ -0,0 +1,50 @@
+//GODLEON'S CODE
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ff                first
+#define ss                second
+#define int        	  long long
+#define ll                long long
+#define ddouble           long double  
+#define pb 		  push_back
+#define mp 		  make_pair
+#define pii               pair<int , int>
+#define vi                vector<int>
+#define mii               map<int,int>
+#define gsort             greater<int>
+#define pqb               priority_queue<int>
+#define pqs               priority_queue<int , vi , greater<int>>
+#define setbits(x)        __builtin_popcountll(x)
+#define zrobits(x)        __builtin_ctzll(x)
+#define mod               1000000007
+#define mod1              mod
+#define mod2              1000000009
+#define inf               1e18
+#define ps(x,y)           fixed<<setprecision(y)<<x
+#define mk(arr,n,type)    type *arr = new type[n] ;
+#define test(t)           int t ; cin >> t ; while(t--)
+#define ifor(a,b)         for(int i = a ; i < b ; i++)
+#define dfor(a,b)         for(int i = b-1 ; i >= a ; i--)
+#define fio               ios_base::sync_with_stdio(0) ; cin.tie(0) ; cout.tie(0) ;
+mt19937                   rng(chrono::steady_clock::now().time_since_epoch().count());
+
+
+
+void solve(){
+    int a,b,c;
+    cin>>a>>b>>c;
+    cout<<a+b+c<<" "<<b+c<<" "<<c<<endl;
+}
+
+int32_t main()
+{
+    fio;
+	int t =1;
+	cin>>t;
+
+    while(t--)
+    {
+		solve();
+    }
+}
