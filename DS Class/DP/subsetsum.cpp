a8bf2541052f93bbb5d4e275225270eedcc05020
Ansh Anurag <anxhnurag@gmail.com>
2022-05-28 22:39:41 +0530

    Subset sum in 1D

---
+++ DS Class/DP/subsetsum.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define vi vector<int>
+#define vvb vector<vector<bool>>
+
+
+
+int main(){
+    int n,sum;
+    cin>>n>>sum;
+    vi a(n);
+    
+    for(auto &x : a){
+        cin>>x;
+    }
+    vvb dp(2,vector<bool>(sum+1,-1));
+    for(int i = 0 ; i <= n ; ++i){
+        for(int j = 0 ; j <= sum ; j++){
+
+            if(j==0){
+                dp[i%2][j]=true;
+            }
+            else if(i==0){
+                dp[i%2][j]=false;
+            }
+            else{
+                if(j>=a[i-1]){
+                    dp[i%2][j]=dp[!(i%2)][j]||dp[!(i%2)][j-a[i-1]];
+                }
+                else{
+                    dp[i%2][j]=dp[!(i%2)][j];
+                }
+            }
+
+        }
+    }
+    cout<<dp[n%2][sum];
+}
