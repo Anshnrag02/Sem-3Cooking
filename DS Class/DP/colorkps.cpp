afb74a2201dc5d46841cedfa265d564915f926fa
Ansh Anurag <anxhnurag@gmail.com>
2022-05-30 03:16:24 +0530

    unbound kps

---
+++ DS Class/DP/colorkps.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n,m,x;
+int w[1000000];
+
+int main(){
+    cin>>n>>m>>x;
+    for(int i = 0 ; i < n ; ++i) cin>>w[i];
+    // vector<vector<int>>clr(m);
+    for(int i = 0 ; i < n ; ++i){
+        cin>>c[i];
+    }
+
+}
