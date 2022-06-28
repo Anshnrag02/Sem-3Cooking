ea60c76a3f2334d2a1c39df1ed2b5daf025d9170
Ansh Anurag <anxhnurag@gmail.com>
2022-06-29 02:06:34 +0530

    Learning RQP

---
+++ DS Class/segment tree/2.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+
+int a[200000];
+int t[1048576];
+
+void createST(int start , int end, int idx){
+    if(start==end){
+        t[idx]=a[start];
+        return;
+    }
+
+    int mid = start + (end-start)/2;
+    createST(start,mid,2*idx+1);
+    createST(mid+1,end,2*idx+2);
+    t[idx]=min(t[2*idx+1],t[2*idx+2]);
+
+}
+
+int query(int s, int e, int idx, int l, int r){
+    if(s>r || e<l){
+        return INT_MAX;
+    }
+
+    if( s>=l and e<=r){
+        return t[idx];
+    }
+
+    int mid = s + (e-s)/2;
+    return min(query(s,mid,2*idx+1,l,r),query(1+mid,e,2*idx+2,l,r));
+}
+
+signed main(){
+    
+    int n,t; cin>>n>>t;
+    for(int i = 0 ; i < n ; ++i) cin>>a[i];
+    createST(0,n-1,0);
+    while(t--){
+        int l,r;
+        cin>>l>>r;
+        cout<<query(0,n-1,0,l-1,r-1)<<endl;
+    }
+}
